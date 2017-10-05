d2becf8ae7a1de8cc551cb389026631e16867789
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2017-10-05 12:13:55 +0200

    Oplossing toegevoegd

---
+++ opdracht-conditional-statements-if-deel2.php
@@ -0,0 +1,55 @@
+<?php
+  $cijfer = 2;
+  $dag = "Onbekende dag";
+
+  if ($cijfer == 1)
+  {
+    $dag = "maandag";
+  }
+
+  if ($cijfer == 2)
+  {
+    $dag = "dinsdag";
+  }
+
+  if ($cijfer == 3)
+  {
+    $dag = "woensdag";
+  }
+
+  if ($cijfer == 4)
+  {
+    $dag = "donderdag";
+  }
+
+  if ($cijfer == 5)
+  {
+    $dag = "vrijdag";
+  }
+
+  if ($cijfer == 6)
+  {
+    $dag = "zaterdag";
+  }
+
+  if ($cijfer == 7)
+  {
+    $dag = "zondag";
+  }
+
+  $dag = strtoupper($dag);
+  $laatsteApositie = strrpos($dag, "A");
+  $dag = substr_replace($dag, "a", $laatsteApositie, 1) #Vervang in $dag de letter door a, als de letter gelijk is aan de laatsteApositie voor 1 keer
+?>
+
+<!doctype html>
+<html>
+  <head>Opdracht: conditional statements if deel 2</head>
+
+  <body>
+    <h1>Opdracht: conditional statements if deel 2</h1>
+
+    <p>Vandaag is het <?php echo ($cijfer == 2) ? $dag : ".";?></p>
+
+  </body>
+</html>
