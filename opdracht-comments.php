21a72dc9bb16512028a9897f3436c41846ce2b4c
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2017-10-05 10:29:16 +0200

    Oplossing toegevoegd

---
+++ opdracht-comments.php
@@ -0,0 +1,19 @@
+<?php
+  /*
+  * Lore Vanderlinden
+  * lore.vanderlinden@student.kdg.be
+  */
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>Opdracht comments</title>
+  </head>
+
+  <body>
+    <h1>Opdracht comments</h1>
+
+    <p><?php echo "Lore Vanderlinden" ?><p>
+  </body>
+</html>
