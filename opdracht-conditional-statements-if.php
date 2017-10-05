7ff8f9af4d0a6cd7f4effd2eeee0b70acffe6a4c
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2017-10-05 12:00:23 +0200

    Oplossing toegevoegd

---
+++ opdracht-conditional-statements-if.php
@@ -0,0 +1,51 @@
+<?php
+  $cijfer = 1;
+
+  if ($cijfer == 1)
+  {
+    $dag = "maandag";
+  }
+
+  if ($cijfer == 2)
+  {
+    $dag = "dinsdag";
+  }
+
+  if ($cijfer == 3)
+  {
+    $dag = "woensdag";
+  }
+
+  if ($cijfer == 4)
+  {
+    $dag = "donderdag";
+  }
+
+  if ($cijfer == 5)
+  {
+    $dag = "vrijdag";
+  }
+
+  if ($cijfer == 6)
+  {
+    $dag = "zaterdag";
+  }
+
+  if ($cijfer == 7)
+  {
+    $dag = "zondag";
+  }
+?>
+
+<!doctype html>
+<html>
+  <head>Opdracht: conditional statements if</head>
+
+  <body>
+    <h1>Opdracht: conditional statements if deel 1</h1>
+
+    <p>Vandaag is het <?php echo ($cijfer == 4) ? $dag : ".";?></p>
+    <p>Vandaag is het <?php echo ($cijfer == 1) ? $dag : ".";?></p>
+
+  </body>
+</html>
