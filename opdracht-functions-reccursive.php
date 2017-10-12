f4f75812140ee6de8084e8841493d4eda8f032aa
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2017-10-12 08:15:02 +0200

    Oplossing toegevoegd

---
+++ opdracht-functions-reccursive.php
@@ -0,0 +1,47 @@
+<?php
+  $hansErfenis = 100000;
+  $rente = 8;
+  $aantalJaar = 10;
+
+
+  function kapitaalBerekenen ($kapitaal, $renteProcent, $jaren)
+  {
+    static $startJaar = 1;
+    static $antwoordArray = array();
+
+    if ($startJaar <= $jaren)
+    {
+      $rentebedrag = $kapitaal * ($renteProcent / 100);
+      $afgerondBedrag = floor($rentebedrag);
+      $nieuwKapitaal = $kapitaal + $afgerondBedrag;
+      $antwoordArray[$startJaar] = "Het nieuwe bedrag is: " . $nieuwKapitaal . " euro met als rente " . $afgerondBedrag. " euro";
+
+      $startJaar++;
+
+      return kapitaalBerekenen($nieuwKapitaal, $renteProcent, $jaren);
+    }
+    else
+    {
+      return $antwoordArray;
+    }
+  }
+
+$winst = kapitaalBerekenen($hansErfenis, $rente, $aantalJaar)
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>Opdracht functions recursive</title>
+
+  </head>
+  <body>
+    <h1>Opdracht functions recursive</h1>
+
+    <ul>
+      <?php foreach ($winst as $value): ?>
+        <li><?php echo $value ?></li>
+      <?php endforeach ?>
+  </ul>
+  </body>
+</html
---
+++ opdracht-functions-recursive-deel2.php
@@ -0,0 +1,42 @@
+<?php
+  function kapitaalBerekenen ($kapitaalArray)
+  {
+    if ($kapitaalArray['startjaar'] <= $kapitaalArray ['jaren'])
+    {
+      $rentebedrag = floor($kapitaalArray['kapitaal'] * ($kapitaalArray['renteprocent'] / 100));
+      $kapitaalArray['kapitaal'] += $rentebedrag;
+      $kapitaalArray['antwoordArray'][$kapitaalArray['startjaar']] = 'Het nieuwe bedrag is: ' . $kapitaalArray['kapitaal'] . ' euro met als rente ' . $rentebedrag . ' euro';
+
+      ++$kapitaalArray['startJaar'];
+
+      return kapitaalBerekenen($kapitaalArray);
+    }
+    else
+    {
+      return $kapitaalArray;
+    }
+  }
+
+$hansErfenis = 100000;
+$rente = 8;
+$aantalJaar = 10;
+
+$winst = kapitaalBerekenen(array('kapitaal' => $hansErfenis, 'renteprocent' => $rente, 'jaren' => $aantalJaar, 'startjaar' => 1, 'antwoordArray' => array()));
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>Opdracht functions recursive</title>
+
+  </head>
+  <body>
+    <h1>Opdracht functions recursive</h1>
+
+    <ul>
+      <?php foreach ($winst['antwoordArray'] as $value): ?>
+        <li><?php echo $value ?></li>
+      <?php endforeach ?>
+  </ul>
+  </body>
+</html
