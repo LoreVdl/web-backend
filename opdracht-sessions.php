a744053137db1a3cdf891d06f9507003ca6cda58
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2017-10-26 06:54:30 +0200

    Oplossing toegevoegd

---
+++ opdracht-sessions.php
@@ -0,0 +1,29 @@
+<?php
+  session_start();
+  var_dump($_SESSION);
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>Opdracht Sessions</title>
+  </head>
+  <body>
+    <h1>Opdracht sessions</h1>
+
+    <form>
+      <ul>
+        <li>
+          <label for="email">Email</label>
+          <input type="text" id="email" value="<?= (isset($_SESSION['email']) ? $_SESSION['email'] : '') ?>" required>
+        </li>
+
+        <li>
+          <label for="nickname">Nickname</label>
+          <input type="text" id="nickname" value="<?= (isset($_SESSION['nickname']) ? $_SESSION['nickname'] : '') ?>" required>
+        </li>
+      </ul>
+      <input type="submit" value="volgende">
+    </form>
+  </body>
+</html>
