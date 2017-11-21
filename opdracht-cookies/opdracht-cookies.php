bd5480c1461fee94397408aab1dfe6323e607c82
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2017-11-21 23:06:05 +0100

    Oplossing toegevoegd

--- opdracht-cookies/opdracht-cookies.php
+++ opdracht-cookies/opdracht-cookies.php
@@ -1,22 +1,30 @@
 <?php
-	if(isset($_GET['logout'])){
+	if(isset($_GET['logout']))
+  {
 		setcookie('authenticated', '', time() -1000);
 		header('location: opdracht-cookies.php');
 	}
-	$fileContent = file_get_contents('opdracht-cookies.txt');
+	$fileContent	=	file_get_contents('cookies.txt');
 	$userData = explode(',', $fileContent);
 	$statusMessage = false;
 	$isAuthenticated = false;
-	if(!isset($_COOKIE['authenticated'])){
-		if(isset($_POST['submit'])){
-			if($_POST['useranme'] == $userData[0] && $_POST['password'] == $userData[1]){
+	if(!isset($_COOKIE['authenticated']))
+  {
+		if(isset($_POST['submit']))
+    {
+			if($_POST['username'] == $userData[0] && $_POST['password'] == $userData[1])
+      {
 				setcookie('authenticated', true, time() + 3600);
 				header('location: opdracht-cookies.1.php');
-			} else {
+			}
+      else
+      {
 				$statusMessage = 'There was an error. Try again.';
 			}
 		}
-	} else {
+	}
+  else
+  {
 		$statusMessage = 'You are logged in.';
 		$isAuthenticated = true;
 	}
@@ -26,19 +34,19 @@
 <head></head>
 	<body>
 		<h1>Inloggen</h1>
-		<?php if ($message): ?>
-			<?=	$message ?>
+		<?php if ($statusMessage): ?>
+			<?=	$statusMessage ?>
 		<?php endif ?>
 		<?php if ( !$isAuthenticated ): ?>
 			<form action="opdracht-cookies.php" method="POST">
 				<ul>
 					<li>
 						<label for="username">Username: </label>
-						<input type="text" name="username" id="username" value="lennert">
+						<input type="text" name="username" id="username" value="jan">
 					</li>
 					<li>
 						<label for="password">Password: </label>
-						<input type="password" name="password" id="password" value="myPass">
+						<input type="password" name="password" id="password" value="paswoord01">
 					</li>
 				</ul>
 				<input type="submit" name="submit" value="login">
