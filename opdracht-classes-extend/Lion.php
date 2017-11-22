c3139756a184615ad30e6b7f753bf1768f18a77a
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2017-11-22 16:08:41 +0100

    Oplossing toegevegoed

---
+++ opdracht-classes-extend/Lion.php
@@ -0,0 +1,16 @@
+<?php
+  class Lion extends Animal {
+    protected $species;
+    protected $go = "roar";
+    public function __construct($name, $gender, $health, $species) {
+      parent::__construct($name, $gender, $health, $species);
+      $this->species = $species;
+    }
+    public function getSpecies() {
+      return $this->species;
+    }
+    public function doSpecialMove() {
+      return $this->go;
+    }
+  }
+?>
