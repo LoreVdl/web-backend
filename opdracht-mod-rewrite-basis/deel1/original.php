754ed45b9593a8083d0ed86f0250a092ccd0baea
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2017-12-15 11:07:58 +0100

    Oplossing toegevoegd

---
+++ opdracht-mod-rewrite-basis/deel1/original.php
@@ -0,0 +1,9 @@
+<!doctype>
+<html>
+  <head>
+    <title>Opdracht mod_rewrite basis</title>
+  </head>
+  <body>
+    <h1>Het originele bestand</h1>
+  </body>
+</html>
---
+++ opdracht-mod-rewrite-basis/deel1/redirect.php
@@ -0,0 +1,9 @@
+<!doctype>
+<html>
+  <head>
+    <title>Opdracht mod_rewrite basis</title>
+  </head>
+  <body>
+    <h1>Het redirect bestand</h1>
+  </body>
+</html>
