afc013ee5483dd299961cf7bd3c1bdc270bafc0e
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2017-12-15 11:25:38 +0100

    Oplossing toegevoegd

---
+++ opdracht-mod-rewrite-basis/deel3/index.php
@@ -0,0 +1,12 @@
+<!doctype>
+<html>
+  <head>
+    <title>Opdracht mod_rewrite basis</title>
+  </head>
+  <body>
+    <h1>var_dump() van $_GET variabele</h1>
+    <code>
+      <?php var_dump($_GET)?>
+    </code>
+  </body>
+</html>
